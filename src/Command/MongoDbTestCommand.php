66dde06d369edb94ee81e52e653a17564ab7d25d
j-christophe <jean-christophe.lassus@sanilea.fr>
2024-11-13 10:59:00 +0100

    Rename for more understanding

--- src/Command/MongoDbTestCommand.php
+++ src/Command/MongoDbTestCommand.php
@@ -14,7 +14,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(name: 'app:mongo_db', description: 'Connection test')]
-class MongoDbCommand extends Command
+class MongoDbTestCommand extends Command
 {
     private TaskRepository $taskRepository;
     private MongoDbService $mongoDbService;
